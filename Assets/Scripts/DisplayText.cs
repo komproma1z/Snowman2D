cd2b997fb40a2ad91dd537a1ad12709908661a21
Alex <kanin.alexey@gmail.com>
2019-02-26 13:42:01 +0700

    First commit

---
+++ Assets/Scripts/DisplayText.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayText : MonoBehaviour
+{
+
+    private enum UpDown { Down = -1, Start = 0, Up = 1 };
+    private Text text;
+    private UpDown textChanged = UpDown.Start;
+
+    void Awake()
+    {
+        // Load the Arial font from the Unity Resources folder.
+        Font arial;
+        arial = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
+
+        // Create Canvas GameObject.
+        GameObject canvasGO = new GameObject();
+        canvasGO.name = "Canvas";
+        canvasGO.AddComponent<Canvas>();
+        canvasGO.AddComponent<CanvasScaler>();
+        canvasGO.AddComponent<GraphicRaycaster>();
+
+        // Get canvas from the GameObject.
+        Canvas canvas;
+        canvas = canvasGO.GetComponent<Canvas>();
+        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+
+        // Create the Text GameObject.
+        GameObject textGO = new GameObject();
+        textGO.transform.parent = canvasGO.transform;
+        textGO.AddComponent<Text>();
+
+        // Set Text component properties.
+        text = textGO.GetComponent<Text>();
+        text.font = arial;
+        text.text = "Press ANY key for the snowman to start running \n and do the same so it stops";
+        text.fontSize = 16;
+        text.alignment = TextAnchor.UpperCenter;
+
+        // Provide Text position and size using RectTransform.
+        RectTransform rectTransform;
+        rectTransform = text.GetComponent<RectTransform>();
+        rectTransform.localPosition = new Vector3(0, 0, 0);
+        rectTransform.sizeDelta = new Vector2(600, 205);
+    }
+}
---
+++ Assets/Scripts/SnowmanMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowmanMovement : MonoBehaviour
+{
+
+    private float moveHorizontal = 1;
+    private float moveVertical = 1;
+    public float speed;
+    private bool isKeyPressed = false;
+    private bool hasCollided = false;
+
+    void FixedUpdate()
+    {
+        if (Input.anyKeyDown)
+        {
+            isKeyPressed = !isKeyPressed;
+            
+        }
+
+        if (isKeyPressed)
+        {
+           transform.Translate(moveHorizontal * 0.2f * speed, moveVertical * 0.045f * speed, 0);
+        }
+
+        if (hasCollided)
+        {
+            moveHorizontal = moveHorizontal * -1;
+        }
+
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Wall") == true)
+        {
+            hasCollided = !hasCollided;
+        }
+
+    }
+
+}
